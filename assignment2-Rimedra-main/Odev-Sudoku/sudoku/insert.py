5d1130d6d5ce9e7155dc296353fc529b4ef11220
Tunahan Demir <48627993+Rimedra@users.noreply.github.com>
2024-01-13 19:01:59 +0300

    Update insert.py

--- assignment2-Rimedra-main/Odev-Sudoku/sudoku/insert.py
+++ assignment2-Rimedra-main/Odev-Sudoku/sudoku/insert.py
@@ -1,23 +1,60 @@
 import hashlib
 import json
 
+def _isValidGrid(grid):
+    isGrid = True
+    for entry in grid:
+        if entry.isalpha():
+            isGrid = False
+            return isGrid
+    gridArray = json.loads(grid)
+    if len(gridArray) != 81:
+        isGrid = False 
+        return isGrid
+    return isGrid
+
 def _insert(parms):
     result = {'status': 'ok'}
+    
+  
+    if 'cell' not in parms:
+        result['status'] = 'error: eksik hücre adresi'
+        return result
+    if len(parms['cell']) != 4:
+        result['status'] = 'error: geçersiz hücre adresi'
+        return result
+    if not parms['cell'][1].isnumeric() or not parms['cell'][3].isnumeric():
+        result['status'] = 'error: geçersiz hücre adresi'
+        return result
+    if parms['cell'][0] not in ('r', 'R') or parms['cell'][2] not in ('c', 'C'):
+        result['status'] = 'error: geçersiz hücre adresi'
+        return result 
+    if int(parms['cell'][1]) < 1 or int(parms['cell'][3]) < 1:
+        result['status'] = 'error: geçersiz hücre adresi'
+        return result
 
-    if 'grid' in parms and 'position' in parms and 'value' in parms:
-        grid = parms['grid']
-        position = int(parms['position'])
-        value = int(parms['value'])
+    if 'grid' not in parms:
+        result['status'] = 'error: eksik grid'
+        return result
+    if not _isValidGrid(parms['grid']):
+        result['status'] = 'error: geçersiz grid'
+        return result
+    
+    rowNumber = int(parms['cell'][1])
+    columnNumber = int(parms['cell'][3])
 
-        if 0 <= position < len(grid) and 1 <= value <= 9:
-            if grid[position] == 0:
-                grid[position] = value
-                result['integrity'] = hashlib.sha256(json.dumps(grid).encode()).hexdigest()
-            else:
-                result['status'] = 'hata: pozisyon dolu'
-        else:
-            result['status'] = 'hata: yanlıs pozisyon ya da deger'
+    if _isCellAHint(parms['grid'], rowNumber, columnNumber):
+        result['status'] = 'error: sabit olarak belirlenmiş bir hücreye değer ekleme girişiminde bulunuldu'
+        return result
+
+    if 'value' in parms:
+        returnGrid = _insertValue(parms['grid'], int(parms['value']), rowNumber, columnNumber)
+        result['grid'] = returnGrid
+        result['status'] = 'ok'
+        gridToHash = json.dumps(returnGrid)
+        result['integrity'] = _calculateHash(gridToHash)
     else:
-        result['status'] = 'hata: eksik parametre'
+        result['status'] = 'error: eksik parametre'
 
     return result
+
